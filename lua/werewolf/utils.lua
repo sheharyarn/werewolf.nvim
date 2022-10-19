55e66d88a6a9ce2698e29648498e259d71be5f5c
mjacq <>
2022-10-19 20:37:11 +0200

    use system mode only on macos. otherwise fallback to fixed hours

--- lua/werewolf/utils.lua
+++ lua/werewolf/utils.lua
@@ -4,15 +4,11 @@
 
 local Utils = {}
 
-
-
 --- Returns the OS name
 -- @return string: 'Darwin' | 'Windows' | 'Linux'
 Utils.get_os = function()
   return vim.loop.os_uname().sysname
 end
-
-
 
 --- Run a system command and return the output
 -- @param  command string: Command to execute
@@ -25,57 +21,55 @@ Utils.run_command = function(command)
   return result
 end
 
-
-
 --- Returns the OS theme using default handlers for each OS
 -- @return string: 'Light' | 'Dark'
 
 local default_theme_handlers = {
   -- Default implmentation for macOS
   Darwin = function()
-    local command = 'defaults read -g AppleInterfaceStyle 2>/dev/null'
-    local result = Utils.run_command(command):gsub('%s', '')
+    local command = "defaults read -g AppleInterfaceStyle 2>/dev/null"
+    local result = Utils.run_command(command):gsub("%s", "")
 
-    if result == 'Dark' then
-      return 'Dark'
+    if result == "Dark" then
+      return "dark"
     else
-      return 'Light'
+      return "light"
     end
   end,
+
   -- Use a fixed hour schedule to set the theme
   -- @param table indicating the default time window to use the light theme
   -- @return 'Light' or 'Dark'
   fixed_hours = function(day_time)
     local time = tonumber(os.date("%H"))
+
     if time >= day_time.from and time < day_time.to then
-      return 'Light'
+      return "light"
     else
-      return 'Dark'
+      return "dark"
     end
   end,
 }
 
-
+local is_macos = vim.fn.has("mac")
 
 Utils.get_theme = function(mode, day_time)
   local handler = nil
 
-  if mode == 'system' then
+  if mode == "system" and is_macos then
     local os = Utils.get_os()
     handler = default_theme_handlers[os]
-    if type(handler) == 'function' then
+    if type(handler) == "function" then
       return handler()
     else
-      print('[ERROR] Could not fetch system theme!')
+      print("[ERROR] Could not fetch system theme!")
     end
-  elseif mode == 'fixed_hours' then
-    handler = default_theme_handlers['fixed_hours']
+  else -- elseif mode == 'fixed_hours' then
+    handler = default_theme_handlers["fixed_hours"]
     return handler(day_time)
-  else
-    print("[ERROR] Unsupported mode!")
+    -- else
+    --   print("[ERROR] Unsupported mode!")
   end
 end
-
-
 
 return Utils
