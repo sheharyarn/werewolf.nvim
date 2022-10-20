908a9c5ba616a115edda5c4ca964723fcb932a32
mjacq <>
2022-10-20 10:15:32 +0200

    small simplification

--- lua/werewolf/utils.lua
+++ lua/werewolf/utils.lua
@@ -10,17 +10,6 @@ Utils.get_os = function()
   return vim.loop.os_uname().sysname
 end
 
---- Run a system command and return the output
--- @param  command string: Command to execute
--- @return string: STDOUT result
-Utils.run_command = function(command)
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  handle:close()
-
-  return result
-end
-
 --- Returns the OS theme using default handlers for each OS
 -- @return string: 'Light' | 'Dark'
 
@@ -28,7 +17,7 @@ local default_theme_handlers = {
   -- Default implmentation for macOS
   Darwin = function()
     local command = "defaults read -g AppleInterfaceStyle 2>/dev/null"
-    local result = Utils.run_command(command):gsub("%s", "")
+    local result = vim.fn.system(command):gsub("%s", "")
 
     if result == "Dark" then
       return "dark"
