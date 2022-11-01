35c4373f89f7d6ca81a000fcb631d55aaf990f34
mjacq <>
2022-11-01 16:43:21 +0100

    fix default to day_time if sunshine not installed

--- lua/werewolf/init.lua
+++ lua/werewolf/init.lua
@@ -7,82 +7,82 @@ local Werewolf = {}
 
 -- Default configuration for Werewolf
 local DEFAULT_OPTS = {
-  on_change = nil,
-  run_on_start = true,
-  period = 500,
-  mode = "system",
-  day_time = { from = 8, to = 20 },
-  location = "#Paris, FR",
+	on_change = nil,
+	run_on_start = true,
+	period = 500,
+	mode = "system",
+	day_time = { from = 8, to = 20 },
+	location = "#Paris, FR",
 }
 
 -- User configuration currently loaded
 local user_opts = DEFAULT_OPTS
 local state = {
-  appearance = nil, -- 'dark' | 'light'
+	appearance = nil, -- 'dark' | 'light'
 }
 local update_appearance = nil
 
 --- Return the werewolf config currently set
 -- @return table: Current Werewolf config
 Werewolf.config = function()
-  return user_opts
+	return user_opts
 end
 
 --- Setup and start Werewolf with the provided config
 -- @param opts table: Config for Werewolf
 -- @return nil
 Werewolf.setup = function(opts)
-  -- Merge user options with default config
-  user_opts = vim.tbl_deep_extend("force", DEFAULT_OPTS, opts or {})
+	-- Merge user options with default config
+	user_opts = vim.tbl_deep_extend("force", DEFAULT_OPTS, opts or {})
 
-  -- Update appearance callback
-  -- @param target_appearance: 'light' | 'dark'
-  update_appearance = function(target_appearance)
-    -- Apply user method if theme changes
-    if target_appearance ~= state.appearance then
-      state.appearance = target_appearance
-      if type(user_opts.on_change) == "function" then
-        user_opts.on_change(state.appearance)
-      end
-    end
-  end
+	-- Update appearance callback
+	-- @param target_appearance: 'light' | 'dark'
+	update_appearance = function(target_appearance)
+		-- Apply user method if theme changes
+		if target_appearance ~= state.appearance then
+			state.appearance = target_appearance
+			if type(user_opts.on_change) == "function" then
+				user_opts.on_change(state.appearance)
+			end
+		end
+	end
 
-  -- Run on start if enabled
-  if user_opts.run_on_start then
-    -- should be run synchronously on startup
-    -- to avoid the fast color change "glitch" effect
-    Werewolf.run({ sync = true })
-  end
+	-- Run on start if enabled
+	if user_opts.run_on_start then
+		-- should be run synchronously on startup
+		-- to avoid the fast color change "glitch" effect
+		Werewolf.run({ sync = true })
+	end
 
-  -- Start execution loop using vim.loop timer
-  local timer = vim.loop.new_timer()
-  local period = user_opts.period
+	-- Start execution loop using vim.loop timer
+	local timer = vim.loop.new_timer()
+	local period = user_opts.period
 
-  timer:start(period, period, vim.schedule_wrap(Werewolf.run))
+	timer:start(period, period, vim.schedule_wrap(Werewolf.run))
 end
 
-local sunshine_executable = vim.fn.executable("sunshine")
+local sunshine_executable = vim.fn.executable("sunshine") ~= 0
 --- Runs Werewolf manually, applying any configurations based
 -- on the system theme
 -- @param run_opts table: run options (sync/async)
 -- @return nil
 Werewolf.run = function(run_opts)
-  run_opts = run_opts or { sync = false }
-  local mode = user_opts.mode
-  local day_time = user_opts.day_time
-  local handler = nil
+	run_opts = run_opts or { sync = false }
+	local mode = user_opts.mode
+	local day_time = user_opts.day_time
+	local handler = nil
 
-  if mode == "system" and Utils.os_uname == "Darwin" then
-    handler = Utils.default_theme_handlers["system"][Utils.os_uname]
-    handler(update_appearance, run_opts)
-  elseif mode == "sunshine" and sunshine_executable then
-    handler = Utils.default_theme_handlers["sunshine"]
-    handler(user_opts.location, update_appearance, run_opts)
-  else
-    -- mode == "fixed_hours" is the fallback
-    handler = Utils.default_theme_handlers["fixed_hours"]
-    handler(day_time, update_appearance)
-  end
+	if mode == "system" and Utils.os_uname == "Darwin" then
+		handler = Utils.default_theme_handlers["system"][Utils.os_uname]
+		handler(update_appearance, run_opts)
+	elseif mode == "sunshine" and sunshine_executable then
+		handler = Utils.default_theme_handlers["sunshine"]
+		handler(user_opts.location, update_appearance, run_opts)
+	else
+		-- mode == "fixed_hours" is the fallback
+		handler = Utils.default_theme_handlers["fixed_hours"]
+		handler(day_time, update_appearance)
+	end
 end
 
 return Werewolf
